412f64dbff2e9fc3e944278eff66ebcc68450f05
Sanid1707 <92554733+Sanid1707@users.noreply.github.com>
2023-02-13 23:15:53 +0000

    deleting the following

--- include/header.php
+++ include/header.php
@@ -10,7 +10,7 @@
     <!-- Bootstrap core CSS -->
 <link href="css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom styles for this template -->
-    <link href="mystyle.css" rel="stylesheet">
+    <link href="./mystyle.css" rel="stylesheet">
   </head>
   <body>
     
@@ -40,14 +40,3 @@
         </div>
       </div>
     </nav>
-
-<main class="container">
-  <div class="starter-template text-center">
-    <h1>Bootstrap starter template</h1>
-    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-  </div>
-
-</main><!-- /.container -->
-    <script src="js/bootstrap.bundle.min.js"></script>
-  </body>
-</html>
