f394a981182255db2ac55e12ee749407512de78c
Sanid1707 <92554733+Sanid1707@users.noreply.github.com>
2023-02-13 10:58:07 +0000

    Made a include file and replace the code from every php file

---
+++ page-1.php
@@ -0,0 +1,12 @@
+
+<?php include 'include/header.php';?>
+<main class="container">
+  <div class="starter-template text-center">
+    <h1>Page 1</h1>
+    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+  </div>
+
+</main><!-- /.container -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
